4f27be90a40dc2c89b60045e905be6af6978a3be
Sophia Agura <sgtagura@gmail.com>
2026-05-21 21:53:25 +0800

    Fix Railway production env loading

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 use App\Kernel;
 
+$projectDir = dirname(__DIR__);
+if (!is_file($projectDir.'/.env') && ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? getenv('APP_ENV')) === 'prod') {
+    $_SERVER['APP_RUNTIME_OPTIONS'] = ['disable_dotenv' => true];
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
