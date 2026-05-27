92d612aca8b1a6cf2844b9e201aa2aa177192c4f
Sophia Agura <sgtagura@gmail.com>
2026-05-27 11:40:06 +0800

    FCM migration

--- migrations/Version20260527022929.php
+++ migrations/Version20260527022929.php
@@ -14,18 +14,48 @@ final class Version20260527022929 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add Firebase Cloud Messaging token fields to users.';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD fcm_token VARCHAR(512) DEFAULT NULL, ADD fcm_platform VARCHAR(20) DEFAULT NULL, ADD fcm_token_updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        if (!$schema->hasTable('user')) {
+            return;
+        }
+
+        $userTable = $schema->getTable('user');
+
+        if (!$userTable->hasColumn('fcm_token')) {
+            $this->addSql('ALTER TABLE `user` ADD fcm_token VARCHAR(512) DEFAULT NULL');
+        }
+
+        if (!$userTable->hasColumn('fcm_platform')) {
+            $this->addSql('ALTER TABLE `user` ADD fcm_platform VARCHAR(20) DEFAULT NULL');
+        }
+
+        if (!$userTable->hasColumn('fcm_token_updated_at')) {
+            $this->addSql('ALTER TABLE `user` ADD fcm_token_updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        }
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP fcm_token, DROP fcm_platform, DROP fcm_token_updated_at');
+        if (!$schema->hasTable('user')) {
+            return;
+        }
+
+        $userTable = $schema->getTable('user');
+
+        if ($userTable->hasColumn('fcm_token')) {
+            $this->addSql('ALTER TABLE `user` DROP fcm_token');
+        }
+
+        if ($userTable->hasColumn('fcm_platform')) {
+            $this->addSql('ALTER TABLE `user` DROP fcm_platform');
+        }
+
+        if ($userTable->hasColumn('fcm_token_updated_at')) {
+            $this->addSql('ALTER TABLE `user` DROP fcm_token_updated_at');
+        }
     }
 }
